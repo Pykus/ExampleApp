2c61c70b1627d713f1b34a6d48fea3e5294c865d
Przemyslaw Przywarty <przemyslaw.przywarty@outlook.com>
2020-03-29 14:19:52 +0200

    sample button click

--- app/src/main/java/com/example/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.exampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+       Log.i("Info", "Button pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
